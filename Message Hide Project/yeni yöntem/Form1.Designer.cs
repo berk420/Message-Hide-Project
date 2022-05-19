2daba4af14644af5146f90848392a3e7df05a19f
berk gümüş <63772343+berk420@users.noreply.github.com>
2022-05-19 15:30:01 +0300

    Add files via upload

---
+++ Message Hide Project/yeni yöntem/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace yeni_yöntem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox10 = new System.Windows.Forms.ListBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.listBox13 = new System.Windows.Forms.ListBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 258);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(455, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(283, 258);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(290, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "blue pixels byte before hiding";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(289, 172);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 94);
+            this.listBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(289, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "blue pixels after hiding ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(289, 421);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "blue bytes after hiding";
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.ItemHeight = 15;
+            this.listBox7.Location = new System.Drawing.Point(289, 297);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(120, 94);
+            this.listBox7.TabIndex = 19;
+            // 
+            // listBox10
+            // 
+            this.listBox10.FormattingEnabled = true;
+            this.listBox10.ItemHeight = 15;
+            this.listBox10.Location = new System.Drawing.Point(290, 439);
+            this.listBox10.Name = "listBox10";
+            this.listBox10.Size = new System.Drawing.Size(120, 94);
+            this.listBox10.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(290, 154);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(159, 15);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Blue pixels bits before hiding";
+            // 
+            // listBox13
+            // 
+            this.listBox13.FormattingEnabled = true;
+            this.listBox13.ItemHeight = 15;
+            this.listBox13.Location = new System.Drawing.Point(1, 297);
+            this.listBox13.Name = "listBox13";
+            this.listBox13.Size = new System.Drawing.Size(120, 94);
+            this.listBox13.TabIndex = 27;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(1, 279);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(140, 15);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "the pixels of the message";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(1, 439);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 94);
+            this.listBox2.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 417);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 15);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "num. of pixels in which the message is hidden";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 15);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "ımage before hiding";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(455, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 15);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "ımage after hiding";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 559);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.listBox13);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.listBox10);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox10;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ListBox listBox13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Message Hide Project/yeni yöntem/Form1.cs
@@ -0,0 +1,215 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yeni_yöntem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Uniqe random value method
+        Random rastgele = new Random();
+        List<int> usedValues = new List<int>();
+        public int UniqueRandomInt(int min, int max)
+        {
+            int val = rastgele.Next(min, max);
+            while (usedValues.Contains(val))
+            {
+                val = rastgele.Next(min, max);
+            }
+            return val;
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region our message has been parsed into bits to hide inside the picture
+            string message_to_hide = "Hi";
+            byte[] message_convert_to_bit = new byte[message_to_hide.Length * 8];
+            int count_message_bit = 0;
+            for (int i = 0; i < message_to_hide.Length; i++)
+            {
+                byte h = (byte)message_to_hide[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((h & 1) == 1)
+                    {
+                        message_convert_to_bit[count_message_bit] = (byte)(message_convert_to_bit[count_message_bit] | 1);
+                    }
+                    else
+                    {
+                        message_convert_to_bit[count_message_bit] = (byte)(message_convert_to_bit[count_message_bit] & 0);
+                    }
+                    h >>= 1;
+                    listBox13.Items.Add(message_convert_to_bit[count_message_bit]);
+
+                    count_message_bit++;
+
+                }
+            }
+            #endregion
+
+            #region printed the original image to the screen
+            Bitmap img = new Bitmap(@"C:\Users\berkg\Desktop\Lena.tiff");
+            pictureBox1.Image = img;
+            #endregion
+
+            #region took the bit-equivalent of the blue pixels in the image and assigned it to the two-dimensional array
+            int width = img.Width;
+            int height = img.Height;
+            byte[] bites_1D_Blue = new byte[width * height * 8];
+            byte[,] bites_2D_Blue = new byte[width * height, 8];
+            int count = 0;
+            int count2 = 0;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    count2++;
+                    Color pixel = img.GetPixel(j, i);//img.GetPixel(height value, width value)
+                    label1.Text = "Blue pixel value";
+                    listBox1.Items.Add(pixel.B);
+                    byte h = (byte)pixel.B;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if ((h & 1) == 1)
+                        {
+                            bites_1D_Blue[count] = (byte)(bites_1D_Blue[count] | 1);
+                        }
+                        else
+                        {
+                            bites_1D_Blue[count] = (byte)(bites_1D_Blue[count] & 0);
+                        }
+                        h >>= 1;
+                        count++;
+                    }
+                }
+            }
+
+
+            //burada blue pikselleri İŞLEM YAPACAĞIMIZ DİZİYE KOYDUK 
+            int count3 = 0;
+
+            for (int m = 0; m < count2; m++)
+            {
+                for (int n = 0; n <8 ; n++)
+                {
+                    bites_2D_Blue[m, n] = bites_1D_Blue[count3];
+                    count3++;
+                }
+                
+            }
+            #endregion
+
+            #region printed the blue pixels on the screen
+            string print_blue;
+            byte ındex_print_blue = 0;
+            for (int i = 0; i < width*height; i++)
+            {
+                print_blue = bites_2D_Blue[i, 7].ToString() + bites_2D_Blue[i, 6].ToString() + bites_2D_Blue[i, 5].ToString() + bites_2D_Blue[i, 4].ToString() + bites_2D_Blue[i, 3].ToString() + bites_2D_Blue[i, 2].ToString() + bites_2D_Blue[i, 1].ToString() + bites_2D_Blue[i, 0].ToString();
+                ındex_print_blue = Convert.ToByte(print_blue, 2);
+                listBox4.Items.Add(print_blue);
+            }
+            #endregion
+
+            #region randomly put the bits of our message into the bit values of the blue pixels
+            int count_for_hidding = 0;
+            int key = 0;
+            int[] key_arr = new int[count_message_bit];
+            for (int i = 0; i < count2; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (j ==0  && (count_message_bit - 1) != count_for_hidding)
+                    {
+                                              key=UniqueRandomInt(0, count2);
+                        key_arr[count_for_hidding] = key;
+                        listBox2.Items.Add(key_arr[count_for_hidding]);
+                        bites_2D_Blue[key, j] = message_convert_to_bit[count_for_hidding];
+                        count_for_hidding++;
+                    }
+                }
+            }
+            #endregion
+
+            #region wrote the bits of the blue pixels where the message is hidden to the screen
+            string print_blue_after_hiding;
+            byte ındex_print_blue_after_hiding = 0;
+            for (int i = 0; i < width*height; i++)
+            {
+                print_blue_after_hiding = bites_2D_Blue[i, 7].ToString() + bites_2D_Blue[i, 6].ToString() + bites_2D_Blue[i, 5].ToString() + bites_2D_Blue[i, 4].ToString() + bites_2D_Blue[i, 3].ToString() + bites_2D_Blue[i, 2].ToString() + bites_2D_Blue[i, 1].ToString() + bites_2D_Blue[i, 0].ToString();
+                ındex_print_blue_after_hiding = Convert.ToByte(print_blue_after_hiding, 2);
+                listBox7.Items.Add(print_blue_after_hiding);
+            }
+            #endregion
+
+            #region bits that the message is hidden are converted to bytes for printing on the screen
+            string add_byte_to_string = "";
+            string[] piksel_bit_string_arr = new string[width * height];
+            for (int i = 0; i < width * height; i++)
+            {
+                for (int j = 8-1; j>-1; j--)
+                {
+                    add_byte_to_string += bites_2D_Blue[i, j].ToString();
+                    if (j==0)
+                    {
+                        piksel_bit_string_arr[i] = add_byte_to_string;
+                        add_byte_to_string = "";
+                    }
+                }
+            }
+            byte[] after_hiding_pixel_byte = new byte[width * height];
+            for (int i = 0; i < after_hiding_pixel_byte.Length; ++i)
+            {
+                after_hiding_pixel_byte[i] = Convert.ToByte(piksel_bit_string_arr[i].Substring(8 * 0,8),2);
+                listBox10.Items.Add(after_hiding_pixel_byte[i]);
+            }
+
+            #endregion
+
+            #region printing the hidden byte data as a photo
+
+            int[,] img_print = new int[width, height];
+            int hide_message_count = 0;
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(j, i);
+                    byte[] bgra = new byte[] { (byte)after_hiding_pixel_byte[hide_message_count], (byte)pixel.G, (byte)pixel.R, (byte)pixel.A };
+                    img_print[i, j] = BitConverter.ToInt32(bgra, 0);
+                    hide_message_count++;
+                }
+
+            }
+            Bitmap bitmap;
+            unsafe
+            {
+                fixed (int* intPtr = &img_print[0, 0])
+                {
+                    bitmap = new Bitmap(width, height, width*4, PixelFormat.Format32bppRgb, new IntPtr(intPtr));
+                }
+            }
+            pictureBox2.Image = bitmap;
+            #endregion
+        }
+
+       
+    }
+}
+
+
+
